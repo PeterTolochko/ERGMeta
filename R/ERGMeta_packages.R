b2dc693df5f6f2e49d1120cdbeb8762e7ba0154f
Peter <ptolochko@hotmail.com>
2024-03-29 17:04:00 +0100

    fixed some bugs

--- R/ERGMeta_packages.R
+++ R/ERGMeta_packages.R
@@ -1,9 +1,9 @@
-#' @importFrom dplyr mutate select
+#' @importFrom dplyr mutate select left_join
 #' @importFrom brms brm mvbf bf prior
 #' @importFrom ergm ergm control.ergm
 #' @importFrom purrr map_dfr
 #' @importFrom tibble as_tibble tibble
-#' @importFrom tidyr pivot_longer pivot_wider left_join
+#' @importFrom tidyr pivot_longer pivot_wider
 #' @importFrom stats as.formula coef
 #' @importFrom utils head tail
 #' @importFrom rlang enquo quo_name .data
