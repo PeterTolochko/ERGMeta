9f82325fb910b4e78328e624e5d766ffad96bd1d
Peter <ptolochko@hotmail.com>
2025-03-03 12:03:37 +0100

    verbose param for printing network id

--- R/ergm_estimation_functions.R
+++ R/ergm_estimation_functions.R
@@ -102,14 +102,15 @@ ergm_estimation <- function(network, formula, control_settings = control.ergm())
 #'
 #' @param network_list A list of network objects.
 #' @param formula A formula specifying the model to be estimated.
-#' @param control_settings Optional control settings for the estimation process. Default is NULL.
+#' @param control_settings Optional control settings for the estimation process. Default is control.ergm().
+#' @param verbose Logical. If TRUE, prints the index of each network being estimated.
 #' @returns A list containing the estimated network models, removed networks, and estimation information.
 #'
 #' @examples
 #' # networks <- list(net1, net2, net3)
-#' # result <- sequential_estimation(networks, ~edges + nodematch("gender"))
+#' # result <- sequential_estimation(networks, ~edges + nodematch("gender"), verbose = TRUE)
 #' @export
-sequential_estimation <- function(network_list, formula, control_settings = control.ergm()) {
+sequential_estimation <- function(network_list, formula, control_settings = control.ergm(), verbose = TRUE) {
   # Input validation
   if (!is.list(network_list) || any(sapply(network_list, function(x) !inherits(x, "network")))) {
     stop("network_list must be a list of network objects.")
@@ -118,30 +119,35 @@ sequential_estimation <- function(network_list, formula, control_settings = cont
     stop("formula must be a valid ergm formula.")
   }
   
-  # Perform estimation on each network using the updated ergm_estimation function
-  estimated_models <- lapply(network_list, function(net) ergm_estimation(net, formula, control_settings))
+  n_networks <- length(network_list)
+  estimated_models <- vector("list", n_networks)
+  removed_networks <- c()
   
-  # Identify networks that could not be estimated
-  estimation_success <- sapply(estimated_models, function(x) !is.null(x))
-  removed_networks <- which(!estimation_success)
-  
-  if (length(removed_networks) > 0) {
-    estimated_models <- estimated_models[estimation_success]
-    cat("Networks:", paste0(removed_networks, collapse = " "), "could not be estimated and were removed.\n")
-  } else {
-    cat("All networks were estimated successfully!\n")
+  for (i in seq_along(network_list)) {
+    if (verbose) cat("Estimating network", i, "of", n_networks, "\n")
+    model <- ergm_estimation(network_list[[i]], formula, control_settings)
+    if (is.null(model)) {
+      removed_networks <- c(removed_networks, i)
+    } else {
+      estimated_models[[i]] <- model
+    }
   }
   
-  # Additional check for networks with infinite estimates or no variance
+  # Filter out NULL elements (networks that failed estimation)
+  valid_idx <- which(!sapply(estimated_models, is.null))
+  estimated_models <- estimated_models[valid_idx]
+  
+  # Check for networks with infinite estimates or no variance
   infinite_estimates <- sapply(estimated_models, function(x) any(is.infinite(summary(x)$coefficients[, 1])))
   no_variance <- sapply(estimated_models, function(x) any(is.na(summary(x)$coefficients[, 2])))
   remove_indices <- infinite_estimates | no_variance
   
   if (any(remove_indices)) {
-    removed_idx <- which(remove_indices)
-    removed_networks <- c(removed_networks, removed_idx)
+    removed_idx <- valid_idx[which(remove_indices)]
+    removed_networks <- unique(c(removed_networks, removed_idx))
     estimated_models <- estimated_models[!remove_indices]
-    cat("Networks", paste0(removed_idx, collapse = " "), "contain infinite estimates or no variance and were removed.\n")
+    cat("Networks", paste0(removed_idx, collapse = " "), 
+        "contain infinite estimates or no variance and were removed.\n")
     cat("Consider re-parametrizing the networks if you want to keep more networks in.\n")
   }
   
@@ -150,8 +156,8 @@ sequential_estimation <- function(network_list, formula, control_settings = cont
     estimated_models = estimated_models,
     removed_networks = removed_networks,
     estimation_info = list(
-      total_networks = length(network_list),
-      successfully_estimated = sum(estimation_success),
+      total_networks = n_networks,
+      successfully_estimated = length(estimated_models),
       removed_infinite_estimates = sum(infinite_estimates),
       removed_no_variance = sum(no_variance)
     )
